ee0f99b4fc284bb80d0fe2c3709c0afc8f4ebf53
Paul Stothard <stothard@ualberta.ca>
2023-11-11 13:17:58 -0700

    Update test.sh and sample output

--- test.sh
+++ test.sh
@@ -6,3 +6,19 @@ nextflow main.nf \
 --align \
 --blast \
 --outdir test_output
+
+DIR1="test_output"
+DIR2="sample_output"
+
+find "$DIR1" -type f | while read -r file; do
+    # Compute the relative path
+    relative_path="${file#$DIR1/}"
+
+    # Check if the corresponding file exists in DIR2
+    if [ -e "$DIR2/$relative_path" ]; then
+        echo "Comparing $file with $DIR2/$relative_path"
+        diff "$file" "$DIR2/$relative_path"
+    else
+        echo "$relative_path does not exist in $DIR2"
+    fi
+done
